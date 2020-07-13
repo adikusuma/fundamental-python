b2c545ea75459cb6a01d1678bf41a113a20fbd76
adikusuma <adikusuma1402@gmail.com>
2020-07-13 13:50:26 +0700

    belajar membuat iterator

---
+++ belajar-iterator.py
@@ -0,0 +1,13 @@
+#mendefiniskan list
+
+my_list = [1,2,3,4]
+
+#memebuat iterasi dengan fungsi iter()
+my_iter = iter(my_list)
+
+#iterasi dengan next()
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+
